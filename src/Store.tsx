1e5b592e86551bb99ab30b2bf1f6f32c89afa146
c-ehrlich <ehrlich.christopher@gmail.com>
2025-01-20 12:33:43 +0100

    updates are granular, init is broken

--- src/Store.tsx
+++ src/Store.tsx
@@ -2,6 +2,7 @@ import { makeAutoObservable, observable, action, runInAction } from "mobx";
 
 class QueryParamsStore {
   params: Map<string, string>;
+  isInternalUpdate = false;
 
   constructor() {
     makeAutoObservable(this, {
@@ -19,12 +20,24 @@ class QueryParamsStore {
 
   // Synchronize the store with the current URL
   updateFromUrl() {
+    if (this.isInternalUpdate) return; // Skip if this was triggered by our own update
+
     const searchParams = new URLSearchParams(window.location.search);
+    console.log("tktk searchParams", searchParams);
     runInAction(() => {
-      this.params.clear();
-      for (const [key, value] of searchParams.entries()) {
+      const newStuff = searchParams.entries();
+      const newStuffMap = new Map(newStuff);
+      console.log("tktk newStuffMap", newStuff, newStuffMap);
+      // for each key, value pair in the existing params that is not in the new stuff, delete it
+      this.params.forEach((_value, key) => {
+        if (!newStuffMap.has(key)) {
+          this.params.delete(key);
+        }
+      });
+      for (const [key, value] of newStuff) {
         this.params.set(key, value);
       }
+      this.updateUrl();
     });
   }
 
@@ -36,12 +49,14 @@ class QueryParamsStore {
 
   // Update the browser's URL based on the store's state
   updateUrl() {
+    this.isInternalUpdate = true;
     const searchParams = new URLSearchParams();
     for (const [key, value] of this.params.entries()) {
       searchParams.set(key, value);
     }
     const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
     window.history.pushState({}, "", newUrl);
+    this.isInternalUpdate = false;
   }
 
   // Get a specific query parameter
@@ -49,6 +64,9 @@ class QueryParamsStore {
     return this.params.get(key);
   }
 
+  /**
+   * Merge params with the existing params.
+   */
   mergeParams(params: Record<string, string>) {
     Object.entries(params).forEach(([key, value]) => {
       this.params.set(key, value);
@@ -56,11 +74,28 @@ class QueryParamsStore {
     this.updateUrl();
   }
 
+  /**
+   * Set params to the new params (existing params that are not in the new params are deleted)
+   */
   setParams(newParams: Record<string, string>) {
-    this.params.clear();
-    Object.entries(newParams).forEach(([key, value]) => {
-      this.params.set(key, value);
-    });
+    for (const item of [...this.params]) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [key, _value] = item;
+      if (!newParams[key]) {
+        this.params.delete(key);
+      } else {
+        if (this.params.get(key) !== newParams[key]) {
+          this.params.set(key, newParams[key]);
+        }
+      }
+    }
+
+    for (const key in newParams) {
+      if (!this.params.has(key)) {
+        this.params.set(key, newParams[key]);
+      }
+    }
+
     this.updateUrl();
   }
 }
